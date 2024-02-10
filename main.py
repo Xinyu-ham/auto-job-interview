cfe5147343f7720a5985f47d79eeb4dd353990c3
xinyu314159 <xinyu314159@gmail.com>
2024-02-10 15:54:05 +0800

    fix: raise error when rounds < 2

--- main.py
+++ main.py
@@ -8,25 +8,31 @@ parser.add_argument('--voice', type=str, default='shimmer')
 parser.add_argument('--rounds', type=int, default=3, help='Number of rounds in the interview including the Q&A round')
 args = parser.parse_args()
 
+if args.input_ui != 'text':
+    raise NotImplementedError('Only text input is supported at the moment')
+if args.rounds < 2:
+    raise ValueError('Number of rounds should be at least 2')
+
+
 ROUNDS = args.rounds
 VOICE = args.voice
 
-if args.input_ui != 'text':
-    raise NotImplementedError('Only text input is supported at the moment')
+def main(jd_source: str='tmp/job_description.txt') -> None:
+    with open(jd_source, 'r') as f:
+        jd = f.read()
+    interviewer = Interviewer(jd, ROUNDS)
+    speech = SpeechService(voice=VOICE)
 
-with open('tmp/job_description.txt', 'r') as f:
-    jd = f.read()
+    starting_question = interviewer.start_interview()
+    speech.text_to_speech(starting_question.message)
 
-interviewer = Interviewer(jd, ROUNDS)
-speech = SpeechService(voice=VOICE)
+    for _ in range(ROUNDS):
+        if args.input_ui == 'text':
+            reply = input("Your response: ")
+        response = interviewer.respond(reply)
+        speech.text_to_speech(response.message)
 
-starting_question = interviewer.start_interview()
-speech.text_to_speech(starting_question.message)
+    speech.text_to_speech(interviewer.evaluate_interview().message)
 
-for _ in range(ROUNDS):
-    if args.input_ui == 'text':
-        reply = input("Your response: ")
-    response = interviewer.respond(reply)
-    speech.text_to_speech(response.message)
-
-speech.text_to_speech(interviewer.evaluate_interview().message)
+if __name__ == '__main__':
+    main()
