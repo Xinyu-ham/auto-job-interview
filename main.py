9a9ec9e10dc59e20447f681d8b1bda865a0ff4ca
xinyu314159 <xinyu314159@gmail.com>
2024-02-10 15:49:03 +0800

    feat: added arguments to main

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from interview import Interviewer
 from speech_service import SpeechService
+import argparse
 
-ROUNDS = 4
-VOICE = 'shimmer'
+parser = argparse.ArgumentParser()
+parser.add_argument('--input-ui', type=str, default='text', help='Input UI for the interview')
+parser.add_argument('--voice', type=str, default='shimmer')
+parser.add_argument('--rounds', type=int, default=3, help='Number of rounds in the interview including the Q&A round')
+args = parser.parse_args()
+
+ROUNDS = args.rounds
+VOICE = args.voice
+
+if args.input_ui != 'text':
+    raise NotImplementedError('Only text input is supported at the moment')
 
 with open('tmp/job_description.txt', 'r') as f:
     jd = f.read()
@@ -14,7 +24,8 @@ starting_question = interviewer.start_interview()
 speech.text_to_speech(starting_question.message)
 
 for _ in range(ROUNDS):
-    reply = input("Your response: ")
+    if args.input_ui == 'text':
+        reply = input("Your response: ")
     response = interviewer.respond(reply)
     speech.text_to_speech(response.message)
 
